d216cc53de4b33923c8d159ceac5fc29f180dfd1
lucas11200 <lucas11200@hotmail.com>
2019-03-24 13:11:58 -0300

    upload projeto com views e urls

--- pagseguro_api/pagseguro_api/urls.py
+++ pagseguro_api/pagseguro_api/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from locapy.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+	url(r'^$', index),
+    url(r'index$', index,name="index"),
+
 ]
